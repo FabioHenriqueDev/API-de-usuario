41719bfafaddd73e7b7616d2206357484c805155
Fabinho <fabiohenr8queluzsantos@gmail.com>
2025-02-06 22:49:41 -0300

    Subindo Arquivos

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from extensions import db
+from flask_cors import CORS
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app) 
+CORS(app)
 
 from models import Usuario
 
@@ -20,13 +29,63 @@ def create_user():
         email=dados['email']
     )
 
+
     email_existente = db.session.query(Usuario).filter_by(email=dados['email']).first()
     
     if email_existente:
         return jsonify({'Error': 'existing Email'})
+    
+
+     # Configurações do servidor SMTP
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    sender_email = os.environ["email"]
+    sender_password = os.environ["senha_app"]
+
+    # Compondo o e-mail
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Bem-vindo(a) ao nosso sistema! 🎉'
+    msg['From'] = sender_email
+    msg['To'] = dados['email']
+
+
+    html = f"""
+            <html>
+            <body style="font-family: Arial">
+                <h1 style="color: blue;">Olá {dados['nome']},</h1>
+                <img src="https://i.pinimg.com/736x/27/68/3e/27683e7872af4e367e2f27e9efdd1993.jpg" alt="Boas-Vindas" style="width:100%;max-width:600px;height:auto;">
+                <h2>Obrigado por se cadastrar no nosso sistema! Estamos muito felizes em tê-lo(a) conosco.</h2>
+                <h3>Aqui estão algumas coisas que você pode fazer agora:</h3>
+                <ul>
+                    <li>Explorar nossos conteúdos exclusivos.</li>
+                    <li>Personalizar seu perfil e preferências.</li>
+                    <li>Aproveitar as promoções e ofertas especiais.</li>
+                </ul>
+                <h3>Se precisar de ajuda ou tiver alguma dúvida, nossa equipe de suporte está sempre pronta para ajudar. Não hesite em entrar em contato!</h3>
+                <h4>Mais informações sobre o quem desenvolveu essa API clique nesse <a href="https://github.com/FabioHenriqueDev">link</a></h4>
+                <p style="color: gray;">Tenha um ótimo dia!</p>
+            </body>
+            </html>
+        """
+
+    msg.attach(MIMEText(html, 'html'))
 
     db.session.add(usuario)
     db.session.commit()
+    # Enviando o e-mail
+    
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+            smtp.starttls()
+            smtp.login(sender_email, sender_password)
+            smtp.send_message(msg)
+            print('Email Enviado com Sucesso')
+
+    except:
+        print('Erro ao envio do E-mail')
+
+   
+
     return jsonify({'mensage': 'User created'})
 
 
